dd4887c061b4b9f9009f2a8efd044cdc9fdad4af
Ethel C <cofresi.ethel@gmail.com>
2018-03-21 16:29:47 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["me","myself","and","I"]
+  array = ["me","myself","and","am"]
 end
 
 def add_element_to_end_of_array(array, element)
